a6ebfcfc7891e764217ed3148fec3f2478fa2efd
Sakthivel Veerapathiran <shaktheev@gmail.com>
2020-04-09 22:57:11 +0530

    Updated element model

--- permissions/models.py
+++ permissions/models.py
@@ -2,13 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS_CHOICES = [ 
-        ('Requested', 'REQUESTED'),
-        ('Approved', 'APPROVED'),
-        ('Followed Up', 'FOLLOWED UP'),
+        ('Filled', 'FILLED'),
+        ('In Review', 'IN REVIEW'),
+        ('Invoice Requested', 'INVOICE REQUESTED'),
+        ('Killed', 'KILLED'),
+        ('TK from Author', 'TK FROM AUTHOR'),
+        ('Need AU Confirmation', 'NEED AU CONFIRMATION'),
+        ('Need Comp Copy', 'NEED COMP COPY'),
+        ('Need Invoice', 'NEED INVOICE'),
+        ('Need PD Confirmation', 'NEED PD CONFIRMATION'),
+        ('Need Permission', 'NEED PERMISSION'),
+        ('Perm Requested', 'PERM REQUESTED'),
+        ('Photo Shoot', 'PHOTO SHOOT'),
+        ('Processing Image', 'PROCESSING IMAGE'),
+        ('Research', 'RESEARCH'),
+        ('Create Original', 'CREATE ORIGINAL'),
     ]
 SPECIFIED_CHOICES = [ 
+        ('Killed', 'KILLED'),
         ('New', 'NEW'),
         ('Pickup', 'PICKUP'),
+        ('Re-use', 'RE-USE'),
+        ('Revised', 'REVISED'),
     ]
 
 class Book(models.Model):
@@ -39,19 +54,15 @@ class Unit(models.Model):
 class Element(models.Model):
     unit = models.ForeignKey(Unit, null=True, related_name='elements', on_delete=models.CASCADE)
     element_number = models.CharField(max_length=30)
-    specified_as = models.CharField(max_length=15, choices=SPECIFIED_CHOICES)
-    active = models.BooleanField(default=True)
-    requested_on = models.DateField(null=True)
-    granted_on = models.DateField(null=True, blank=True)
+    specified_as = models.CharField(max_length=25, choices=SPECIFIED_CHOICES)
     caption = models.TextField(max_length=300, null=True)
     source = models.CharField(max_length=75, null=True)
-    status = models.CharField(max_length=15, choices=STATUS_CHOICES)
+    credit_line = models.TextField(max_length=300, null=True)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES)
     source_link = models.CharField(max_length=100, null=True)
     title = models.CharField(max_length=100, null=True)
-    author_name = models.CharField(max_length=75, null=True)
-    author_email_id = models.EmailField(null=True)
-    alternative_email_id = models.EmailField(null=True)
-    notes = models.TextField(max_length=300, null=True)
+    rh_email = models.EmailField(null=True)
+    alt_email = models.EmailField(null=True)
     rh_address = models.TextField(max_length=100, null=True)
     phone = models.CharField(max_length=20, null=True)
     fax = models.CharField(max_length=20, null=True)
@@ -59,10 +70,14 @@ class Element(models.Model):
     jbl_rh_name = models.CharField(max_length=75, null=True)
     file_location = models.CharField(max_length=100, null=True)
     file_name = models.CharField(max_length=40, null=True)
+    requested_on = models.DateField(null=True)
+    granted_on = models.DateField(null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(null=True)
     created_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
 
     def __str__(self):
         return self.element_number
