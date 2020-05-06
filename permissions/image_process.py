be200d8a9dffdebbc489af3e5296bbcb88de6912
Sakthivel Veerapathiran <shaktheev@gmail.com>
2020-05-06 10:43:03 +0530

    Changed image location and fixed permission pages'

--- permissions/image_process.py
+++ permissions/image_process.py
@@ -1,23 +1,27 @@
 import os, sys
+import PIL
 from PIL import Image
 from fnmatch import fnmatch
 import os.path
 from os import path
 
 def i_process(isbn, media_path):
-    images='images'
-
+    images = 'resized'
+    basewidth = 450
     #root=input("Enter folder name:")
     root=media_path+"/documents/"+isbn
     if path.exists(root):
         for infile in os.listdir(root):
-            if infile.endswith('.png') or infile.endswith('.tif') or infile.endswith('.jpg') or infile.endswith('.eps'):
+            if infile.endswith('.png') or infile.endswith('.tif') or infile.endswith('.jpg') or infile.endswith('.eps')  or infile.endswith('.gif'):
                 im = Image.open(os.path.join(root,infile))
+                wpercent = (basewidth / float(im.size[0]))
+                hsize = int((float(im.size[1]) * float(wpercent)))
+                im = im.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
                 if not(os.path.exists(os.path.join(root,images))):
                     os.mkdir(os.path.join(root,images))
                 outfile = os.path.join(root,images,os.path.splitext(infile)[0]+".jpg")
                 out = im.convert("RGB")
-                out.save(outfile, "JPEG", quality=90)
+                out.save(outfile, "JPEG")
             # do something
         # return something
         return isbn
